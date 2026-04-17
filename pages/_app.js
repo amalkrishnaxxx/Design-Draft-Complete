f4231b3a7d6c144e3999290316108767a62cce13
Amal Krishna <amalkrishna361@gmail.com>
2026-04-17 23:42:20 +0530

    feat: add favicon using Design Draft logo mark with orange background

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,17 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/sonner";
 import Script from "next/script";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <meta name="theme-color" content="#f97316" />
+      </Head>
       <Script
         src="https://analytics.ahrefs.com/analytics.js"
         data-key="n4yPRZ3tRl9yvvTQ5A903g"
@@ -21,3 +28,4 @@ export default function MyApp({ Component, pageProps }) {
     </>
   );
 }
+
